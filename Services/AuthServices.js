97794f59e6c36f151cca2adcfcdcf79cf4003a1e
Youssef Emad Hussin <57916445+youssefemadhassan66@users.noreply.github.com>
2026-03-17 01:58:41 +0200

    Finshed JWT Helper working on authServices

--- Services/AuthServices.js
+++ Services/AuthServices.js
@@ -1,4 +1,6 @@
 import User from "../Models/user";
+import CatchAsync from "../Utilities/CatchAsync";
+import AppErrorHelper from "../Utilities/AppErrorHelper";
 import { ComparePasswordHelper , hashPasswordHelper } from "../Utilities/HashHelper";
 import crypto from 'crypto'
 
--- Utilities/Jwt.js
+++ Utilities/Jwt.js
@@ -1,16 +1,37 @@
-import { jwt } from "jsonwebtoken";
+import jwt, { sign }  from "jsonwebtoken";
 
- const SignAccessToken = async (user) => {
 
-    try{
-        const payload = {id:user._id,role:user.role};
-        const Secret = process.env.JWT_TOKEN_SECRET;
-        const options = {expiresIn: process.env.JWT_TOKEN_EXPIRES_IN };
-        await jwt.sign(payload,Secret,options)
 
-    }
-    catch(err){
-        console.log("Error while")
-    }
-    
- }
+const signAccessToken  = async(user) => {
+    const accessToken =  jwt.sign(
+        {id:user.id,role:user.role},
+        process.env.JWT_TOKEN_SECRET,
+        {expiresIn:process.env.JWT_TOKEN_EXPIRES_IN}
+    )
+    return accessToken
+}
+const signRefreshToken = async(user) => {
+    const RefreshToken =  jwt.sign(
+        {id:user.id},
+        process.env.JWT_REFRESH_EXPIRES_IN,
+        {expiresIn:process.env.JWT_REFRESH_TOKEN_SECRET}
+    )
+    return RefreshToken
+}
+
+
+const generateToken = async(user)=>{
+    await signAccessToken(user);
+    await signRefreshToken(user);
+}
+
+const verifyAccessToken = (accessToken) =>{
+    return  jwt.verify(accessToken,process.env.JWT_TOKEN_SECRET)
+}
+const verifyRefreshToken = (refreshToken) =>{
+    return  jwt.verify(refreshToken,process.env.JWT_REFRESH_TOKEN_SECRET)
+}
+
+export {
+    signAccessToken,signRefreshToken,generateToken,verifyAccessToken,verifyRefreshToken
+}
