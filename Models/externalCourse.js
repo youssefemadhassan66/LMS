b12972c1520baacf98b47e9348e9101e3d5d334d
Youssef Emad Hussin <57916445+youssefemadhassan66@users.noreply.github.com>
2026-03-30 14:21:36 +0200

    Working on External Course Crud and working with kilo AI to check on Security problems

--- Models/externalCourse.js
+++ Models/externalCourse.js
@@ -5,22 +5,36 @@ const externalCourseSchema = new mongoose.Schema({
     teacher:{
         type:String
     },
-    Subject:{
+    subject:{
         type:String,
         required:true
     },
-    CreatedBy : {
+    createdBy : {
         type:mongoose.Schema.ObjectId,
         ref:"User",
     },
     student:{
         type:mongoose.Schema.ObjectId,
         ref:"User",
+        required:true
     },
-    color: { type: String, default: '#6366f1' }
+    color: { type: String, }
 
 },{timestamps:true})
 
+
+externalCourseSchema.pre('save' , async function () {
+    if (!this.color) {
+        const randomColor ="#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+
+        this.color = randomColor;
+   }
+
+  next();
+    
+})
+
 const ExternalCourse = new mongoose.model("ExternalCourse",externalCourseSchema);
+
 
 export default ExternalCourse
