b40c433b01b6048ff0c1bddac6769a06bfbcc814
Youssef Emad Hussin <57916445+youssefemadhassan66@users.noreply.github.com>
2026-03-31 01:50:56 +0200

    Added 2 important functions for Extrenal Courses (GetMyExtenalCourses and GetMyExtenalCoursesById)

--- Services/ExternalCourseService.js
+++ Services/ExternalCourseService.js
@@ -1,10 +1,11 @@
-import AppErrorHelper from "../Utilities/AppErrorHelper";
-import ExternalCourse from '../Models/ExternalCourse.js'
-import mongoose from "mongoose";
+import AppErrorHelper from "../Utilities/AppErrorHelper.js";
 import ApiFeatures from "../Utilities/ApiFeatures.js";
+import User from "../Models/User.js";
+import ExternalCourse from "../Models/ExternalCourse.js";
+import StudentProfile from "../Models/studentProfile.js";
+import mongoose from "mongoose";
 
-
-const CreateExternalCourse = async(data)=>{
+const createExternalCourseService = async(data)=>{
 
     const {teacher,subject,createdBy,student,color} = {...data};
     
@@ -30,9 +31,79 @@ const CreateExternalCourse = async(data)=>{
         student:student,
         color:color
     });
+}
 
+
+const getMyExternalCourseByIdService  = async(user,courseId)=>{
+    const  exCourse  = await ExternalCourse.findById(courseId)
+    .populate("student", "FullName UserName")
+    .populate("createdBy", "FullName UserName")
+
+    if(!exCourse){
+        throw new AppErrorHelper("Course not found" , 404);
+    }
+    if(user.role === "student"){
+        if(exCourse.student._id.toString()  !== user.id){
+            throw new AppErrorHelper("Not allowed !" , 403);
+        }
+    }
+
+    else if(user.role === "parent"){
+
+        const childProfile = await StudentProfile.find({
+            user:exCourse.student._id,
+            parents: new mongoose.Types.ObjectId(user.id)
+        })
+
+
+        if(!childProfile){
+            throw new AppErrorHelper("Not allowed !" , 403);
+        }
+    }
+    else {
+        throw new AppErrorHelper("Not allowed", 403);
+    } 
+
+    return exCourse;
 
 }
+
+
+const getMyExternalCourseService = async (user,queryString) => {
+  let mongooseQuery;
+
+  if (user.role === "student") {
+    mongooseQuery =  ExternalCourse.find({ student: user.id })
+    .populate("student", "FullName UserName");
+}
+
+else if (user.role === "parent") {
+
+    const childrenProfiles = await StudentProfile.find(
+        {parents: new mongoose.Types.ObjectId(user.id)},
+        {user:1}
+    );
+
+    if(!childrenProfiles.length){
+        return []
+    }
+    
+    const childrenIds = childrenProfiles.map(profile=>profile.user);
+
+        mongooseQuery = await ExternalCourse.find({student:{$in:childrenIds}})
+        .populate("student", "FullName UserName");
+    }
+
+    else throw new AppErrorHelper("Not allowed", 403);
+
+    const features = new ApiFeatures(mongooseQuery,queryString)
+    .filter()
+    .sort()
+    .fields()
+    .pagination();
+
+    return await features.mongooseQuery;
+};
 
 const getAllExternalCoursesService = async(queryString={}) =>{
         
@@ -48,24 +119,23 @@ const getAllExternalCoursesService = async(queryString={}) =>{
 
 const getExternalCourseByIdService  = async(exCourseId) =>{
     
-    const exCourse = ExternalCourse.findById(exCourseId);
+    const exCourse = await ExternalCourse.findById(exCourseId);
 
     if(!exCourse){
         throw new AppErrorHelper("Course not found ! ", 404);
     }
     return exCourse;
-
-
 }
+
 const getExternalCourseByStudentService = async (studentId, queryString = {}) => {
     const features = new ApiFeatures(
-        ExternalCourse.find({ studentId }), 
+        ExternalCourse.find({ student: studentId }), 
         queryString
     ).sort().fields().pagination();
     return await features.mongooseQuery;
 }
 
-const updateExternalService = async(exCourseId,data) =>{
+const updateExternalCourseService = async(exCourseId,data) =>{
 
     const options = {
         new:true,
@@ -82,3 +152,27 @@ const updateExternalService = async(exCourseId,data) =>{
 
 }
 
+const deleteExternalCourseService = async(exCourseId) =>{
+
+    const deletedExCourse = await ExternalCourse.findByIdAndDelete(exCourseId)
+
+    if(!deletedExCourse){
+        throw new AppErrorHelper("Course not found ! " , 404);
+    }
+
+    return deletedExCourse;
+
+}
+
+
+export {
+
+    createExternalCourseService,
+    getMyExternalCourseService,
+    getMyExternalCourseByIdService,
+    getAllExternalCoursesService,
+    getExternalCourseByIdService,
+    getExternalCourseByStudentService,
+    updateExternalCourseService,
+    deleteExternalCourseService
+}
